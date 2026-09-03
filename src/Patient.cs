60f85ee1c794712f40a6fc12fd9d585ab779340f
Branden <claybranden02@gmail.com>
2026-09-03 15:35:30 -0500

    Hospital Project Creation

---
+++ src/Patient.cs
@@ -0,0 +1,10 @@
+
+namespace Hospital
+{
+    internal class Patient
+    {
+        
+    }
+}
+
+
---
+++ src/Program.cs
@@ -0,0 +1,18 @@
+﻿
+namespace Hospital
+{
+
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        
+
+
+
+
+
+        }
+    }
+}
